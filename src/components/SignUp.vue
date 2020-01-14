402089b8c3a85925c2e5daebea1c7479a6609aba
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2020-01-13 21:12:28 -0300

    Modificações na tela de login
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update aperial.css
    
    Co-authored-by: Antônio Pedro <antoniopedro.rs9@gmail.com>

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,12 +2,17 @@
   <div class="cards" oncontextmenu="return false">
     <div class="card">
       <h5>Cadastrar-se</h5>
-      <form onpaste="return false">
-        <input name="Email" type="email" placeholder="Email..." autocomplete="off"/>
+      <form onpaste="return false" onselectstart="return false;">
+        <input name="Email" type="email" placeholder="Email..." autocomplete="off" required/>
+        <select required>
+          <option name="emailInstitucional" value="algarMail">@algartelecom.com.br</option>
+          <option name="outro" value="other">Outro</option>
+        </select>
         <input name="Matricula" type="text" placeholder="Matrícula..." autocomplete="off"/>
         <input name="Senha" type="password" placeholder="Senha..." autocomplete="off"/>
-        <input name="ConfirmarSenha" type="password" placeholder="Confirmar senha..." autocomplete="off"/>
-
+        <input name="ConfirmarSenha" type="password" placeholder="Confirmar senha..." autocomplete="off"/><br>
+        <input name="LembrarUsuario" type="checkbox" value="LembrarUsuario" id="remindUser">
+        <label for="remindUser">Lembrar Usuário</label><br>
         <input class="theme-blue" type="submit" v-on:click="$router.push('/home')"/>
       </form>
       <h6>Já tem uma conta? <a v-on:click="$router.push('/')">Conecte-se</a></h6>
